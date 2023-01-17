ec272b93b00bedec35729778ae500427b1801d2f
vladimir Burdyka <vladimirb.by@gmail.com>
2023-01-17 08:51:36 +0300

    first commit

---
+++ script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+time_delay=3
+
+# INSERT INTO info
+
+docker exec -it replication mysql -uroot -proot -e "USE k8s_temp_env; INSERT INTO info VALUES (NULL,'test','10-01-2021','20-01-2021','active', 'user-test, user-test');"
+
+# output from mysql
+
+output_mysql=$(docker exec -it replication mysql -uroot -proot -e  "USE k8s_temp_env; SELECT json_object('id', id)  FROM info ORDER BY id DESC LIMIT 1;" | awk -F'[^0-9]*' '$0=$2')
+
+echo Privet $output_mysql
+
+
+# output from kafka
+
+sleep $time_delay 
+
+output_kafka=$(bash -c "docker run --network aleks_default confluentinc/cp-kafkacat kafkacat -b kafka:9092  -t DB_k8s_temp_env_info  -o -1 -C -q  -p 0 -e" | jq --raw-output .data.id)
+
+echo Hello $output_kafka
+
+
+if [ "$output_mysql" -eq "$output_kafka" ]
+then
+  echo 1 >> logs.prom
+else
+  echo 0 >> logs.prom
+fi
