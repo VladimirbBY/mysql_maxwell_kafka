c2bf8fc32e3ff8abebfd6b4524ba58e43520dfc8
vladimir Burdyka <vladimirb.by@gmail.com>
2023-01-18 14:43:11 +0300

    finaly script

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 from flask import Response, Flask, render_template
 import os
+import subprocess
 
 print("Hello")
 app = Flask(__name__)
@@ -8,18 +9,17 @@ app = Flask(__name__)
 def metrics():
     try:
         os.system('docker exec -it replication mysql -uroot -proot -e "USE  test_temp_env; INSERT INTO info VALUES (NULL,"test");"')
-        output_mysql = os.system("docker exec -it replication mysql -uroot -proot -e  'USE  test_temp_env; SELECT json_object('id', id)  FROM info ORDER BY id DESC LIMIT 1;' | awk -F'[^0-9]*' '$0=$2'")
-        print(output_mysql)
-        output_kafka = os.system('bash -c "docker run --network mysql_maxwell_kafka_default confluentinc/cp-kafkacat kafkacat -b kafka:9092  -t DB_test_temp_env_info  -o -1 -C -q  -p 0 -e" | jq --raw-output .data.id')
-        print(output_kafka)
-
-        if output_mysql == output_kafka:
-            message = 'connection{Yes="working"} 0'
+        output_mysql = subprocess.check_output("docker exec -it replication mysql -uroot -proot -e  'USE  test_temp_env; SELECT json_object('id', id)  FROM info ORDER BY id DESC LIMIT 1;' | awk -F'[^0-9]*' '$0=$2'", shell=True)
+        output_kafka = subprocess.check_output('bash -c "docker run --network mysql_maxwell_kafka_default confluentinc/cp-kafkacat kafkacat -b kafka:9092  -t DB_test_temp_env_info  -o -1 -C -q  -p 0 -e" | jq --raw-output .data.id', shell=True)
+        print(output_mysql, "*"*50)
+        print(output_kafka, "*"*50)
+        if str(output_mysql.decode()) == str(output_kafka.decode()):
+            message = 'connection 1'
         elif output_mysql != output_kafka:
-            message = 'connection{No="working"} 1'
-        return render_template('index.html', message=message)
+            message = 'connection 0'
+        return Response(message, mimetype="text/plain")
     except(NameError):
-        message = 'connection{Error="working"} 2'
+        message = 'connection 2'
         return Response(message, mimetype="text/plain", status=200)
 
 if __name__ == "__main__":
